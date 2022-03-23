f01dfae144724af643162ce4a8f78add9f695690
Joao Vitor <joaovitorsoares752@gmail.com>
2022-03-22 21:19:54 -0300

    Criando uma versao do projeto em Windows Forms

---
+++ Windows_Calculator/Calculator/MainScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MainScreen : Form
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
